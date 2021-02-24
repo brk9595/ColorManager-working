bffd1baec41e728b17f978c1649836796b810c11
Bharath <bharath1.b@paytmmoney.com>
2021-02-24 22:22:27 +0530

    Initial Commit

---
+++ Shared/ColorManager2App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorManager2App.swift
+//  Shared
+//
+//  Created by Bharath Raj Kumar B on 24/02/21.
+//
+
+import SwiftUI
+
+@main
+struct ColorManager2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
